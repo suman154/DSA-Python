12b13e57a40c11389c7a6697ab93fef0c0f250f9
suman154 <sumanbhatta735@gmail.com>
2024-08-03 06:57:12 +0545

    views and urls create for jwt

--- JWT/authentication/urls.py
+++ JWT/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import WelcomeView 
 
 urlpatterns = [
-    path('', home, name="home"),
+    path('welcome/', WelcomeView.as_view(), name='welcome'),
 ]
--- JWT/authentication/views.py
+++ JWT/authentication/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
-def home(request):
-    return HttpResponse(request,"<h1>Hello</h1>")
+
+class WelcomeView(APIView):
+    # Restrict access to authenticated users only
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        # Response content
+        content = {'message': 'Welcome to the Platform'}
+        # Return the content as a JSON response
+        return Response(content)
